b96caf48bcb93ae807e7a3550a8b60d6f2ee49ef
Jean-Frederic Clere <jfclere@gmail.com>
2022-10-23 16:57:26 +0100

    Add a simple example.

---
+++ Simple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import minimalmodbus
+
+instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 2) # port name, slave address (in decimal)
+instrument.serial.baudrate = 115200
+instrument.serial.stopbits = 2
+instrument.serial.timeout  = 5
+
+## Read temperature (PV = ProcessValue) ##
+temperature = instrument.read_register(0, 2) # Registernumber, number of decimals
+print( temperature )
+if temperature >= 30:
+  instrument.write_register(3,1,1)
+else:
+  instrument.write_register(3,0,1)
+
+temperature = instrument.read_register(1, 2) # Registernumber, number of decimals
+print( temperature )
+
+if temperature >= 30:
+  instrument.write_register(4,1,1)
+else:
+  instrument.write_register(4,0,1)
+
+## Change temperature setpoint (SP) ##
+#NEW_TEMPERATURE = 95
+#instrument.write_register(7, NEW_TEMPERATURE, 1) # Registernumber, value, number of decimals for storage
